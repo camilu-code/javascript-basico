1d781473b6b5dfbd987a538bc0703a62c12ffa54
Camila <asegoviar@est.ups.edu.ec>
2025-06-09 16:44:36 -0500

    Arreglos

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function mostrarMensaje(){
     alert("HOLA MUNDO");
 }
+function imprimirVariables() {
+    let nombre = "Camila"
+    let edad = 24;
+/// forma tradicional  de concatenar cadenas
+    console.log("nombre: ", nombre);
+    console.log("edad: ", edad);
+/// froma moderna de concatenar cademas 
+    console.log(`nombre: ${nombre}, edad ${edad}`);
+    console.error("Error: no funciona");
+    console.warn("Advertencia; zzz");
+}
+
+
+}
